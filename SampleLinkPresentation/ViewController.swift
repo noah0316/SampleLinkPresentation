4138cea6ebca67cc94417bab32a133b15d101bf8
Noah <noa316@naver.com>
2022-02-19 19:15:40 +0900

    Initial commit

---
+++ SampleLinkPresentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleLinkPresentation
+//
+//  Created by Noah on 2022/02/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
